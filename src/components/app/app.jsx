4e59e9f61c3eae64e2b0ade8019d77cc5bae1c37
Egor <egor@serverice.ru>
2020-07-03 23:08:32 +0700

    WIP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import MainScreen from "../main-screen/main-screen.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {rentCount} = props;
+  const rentTitles = props;
   return <MainScreen
-    count = {rentCount}
+    titles = {rentTitles}
   />;
+};
+
+
+App.propTypes = {
+  titles: PropTypes.arrayOf(
+      PropTypes.string.isRequired
+  ).isRequired,
 };
 
 export default App;
