144f3baa7e5e395cd12bae8d09d3d281d24e3c8c
Egor <egor@serverice.ru>
2020-08-04 01:25:58 +0700

    add place-detail component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import MainScreen from "../main-screen/main-screen.jsx";
+import PlaceDetail from "../place-detail/place-detail.jsx";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
 const App = (props) => {
   const {places, onAticleClick} = props;
-  return <MainScreen
-    onAticleClick = {onAticleClick}
-    places = {places}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen
+            onAticleClick = {onAticleClick}
+            places = {places}
+          />;
+        </Route>
+        <Route exact path="/dev-component">
+          <PlaceDetail
+            place = {places[0]}
+          />;
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
---
+++ src/const.js
@@ -0,0 +1,12 @@
+export const PlaceFeatures = {
+  wifi: `Wi-Fi`,
+  washingmachine: `Washing Machine`,
+  towels: `Towels`,
+  heating: `Heating`,
+  coffeemachine: `Coffee machine`,
+  babyseat: `Baby seat`,
+  kitchen: `Kitchen`,
+  dishwasher: `Dishwasher`,
+  cabeltv: `Cabel TV`,
+  fridge: `Fridge`,
+};
