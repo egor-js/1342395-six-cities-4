d8125c301c934ae9d0122d6f7d09fd1c463ee637
Egor <egor@serverice.ru>
2020-08-07 21:32:01 +0700

    module4-task1 complete with tests

--- src/components/place-card/place-card.test.js
+++ src/components/place-card/place-card.test.js
@@ -5,40 +5,6 @@ import testData from "../../mocks/test-data.js";
 
 const onAticleClick = function () {};
 const onCardHover = function () {};
-const mocks = [
-  {
-    photoUrl: `img/apartment-01.jpg`,
-    isPremium: false,
-    price: 50,
-    title: `Beautiful & luxurious apartment at great location`,
-    type: `Apartment`,
-    raiting: 5,
-  },
-  {
-    photoUrl: `img/apartment-02.jpg`,
-    isPremium: true,
-    price: 85,
-    title: `Wood and stone place`,
-    type: `Room`,
-    raiting: 4,
-  },
-  {
-    photoUrl: `img/apartment-03.jpg`,
-    isPremium: false,
-    price: 42,
-    title: `Canal View Prinsengracht`,
-    type: `House`,
-    raiting: 3,
-  },
-  {
-    photoUrl: `img/apartment-01.jpg`,
-    isPremium: false,
-    price: 22,
-    title: `Nice, cozy, warm big bed apartment`,
-    type: `Hotel`,
-    raiting: 2,
-  },
-];
 
 describe(`place card`, () => {
   it(`snapshot test place card`, () => {
